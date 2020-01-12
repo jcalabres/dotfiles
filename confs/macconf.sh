3b0a8a08be713065ee02a9c95e793d74277bb081
jcalabres <jcalabres22@gmail.com>
2020-01-12 17:03:14 +0000

    some macos changes

--- confs/macconf.sh
+++ confs/macconf.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# macos policies enable
 spctl --master-disable
+
 defaults write com.apple.finder CreateDesktop -bool false
+defaults write com.apple.screencapture location ~/Pictures/screenshots/
+defaults write com.apple.screencapture type -string "png"
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 defaults write com.apple.dock autohide-time-modifier -int 0;
-defaults write com.apple.screencapture location ~/Pictures/screenshots/
+defaults write com.apple.dock expose-animation-duration -float 0.12
 
-killall Dock
-killall Finder
+killall Dock Finder
